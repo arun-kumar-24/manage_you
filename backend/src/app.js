654cfb9191c22a756426dd8d0e42b6f8736b854b
arun-kumar-24 <arunkumarm.2645s@gmail.com>
2025-04-05 19:56:12 +0530

    Backend initial setup

---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+app.use(express.json());
+
+export default app;
